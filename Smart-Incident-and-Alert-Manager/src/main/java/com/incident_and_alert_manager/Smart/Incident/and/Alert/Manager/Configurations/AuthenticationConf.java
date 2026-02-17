46122b4e0cc7cab88ab231de0f712f4762d0f5bf
Divyansh Prasad <divyanshprasad01@gmail.com>
2026-02-17 15:01:51 +0530

    Configured Cloud DB successfully and also configured CORS for all websites for now, tested with frontend and working great now need to create frontend for other pages

--- Smart-Incident-and-Alert-Manager/src/main/java/com/incident_and_alert_manager/Smart/Incident/and/Alert/Manager/Configurations/AuthenticationConf.java
+++ Smart-Incident-and-Alert-Manager/src/main/java/com/incident_and_alert_manager/Smart/Incident/and/Alert/Manager/Configurations/AuthenticationConf.java
@@ -13,6 +13,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +33,7 @@ public class AuthenticationConf {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**")
@@ -46,6 +51,20 @@ public class AuthenticationConf {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource url = new UrlBasedCorsConfigurationSource();
+        url.registerCorsConfiguration("/**", corsConfiguration);
+        return url;
     }
 
 
